e9185ab175800769e5b9cf12734bf3f791ad21e4
QAEng2827 <inna.nakopyuk@gmail.com>
2019-07-24 15:53:16 +0300

    Lesson 1 - Begin

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+
+public class BaseTest {
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    @Before
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+}
